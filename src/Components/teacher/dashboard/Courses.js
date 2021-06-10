76e384e7842ca7f32a4cd29b1977353a7ed36801
ATOM03 <priyanshu.jhaginia@albanero.io>
2021-06-11 00:49:57 +0530

    done Course Making

---
+++ src/Components/teacher/dashboard/Courses.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Card, CardActions, CardContent, Typography } from "@material-ui/core";
+import "./Courses.scss";
+
+function Courses(props) {
+  console.log(props.index);
+  return (
+    <div className="courses">
+      <Link to={`/courses/${props.courseCode}`}>
+        <Card raised="true">
+          <h2>{props.courseCode}</h2>
+          <p>{props.courseName}</p>
+          <p>
+            Number of Students : <span>{props.numberOfStudents}</span>
+          </p>
+        </Card>
+      </Link>
+    </div>
+  );
+}
+
+export default Courses;
